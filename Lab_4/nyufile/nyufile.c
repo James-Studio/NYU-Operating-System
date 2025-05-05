ff9a85b38155dddf25d28c807015764d9ab6d68f
James-Studio <hongjames111@gmail.com>
2025-05-05 10:26:01 -0400

    Arrange the code

--- Lab_4/nyufile/nyufile.c
+++ Lab_4/nyufile/nyufile.c
@@ -1,6 +1,11 @@
 /*
-    Reference:
-    StackOverflow: https://stackoverflow.com/questions/3969047/is-there-a-standard-way-of-representing-an-sha1-hash-as-a-c-string-and-how-do-i
+    < Reference >
+
+    StackOverflow: 
+    1. https://stackoverflow.com/questions/3969047/is-there-a-standard-way-of-representing-an-sha1-hash-as-a-c-string-and-how-do-i
+    
+    GeeksforGeeks:
+    1. https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
 */
 
 #include "functions.h"
@@ -9,10 +14,9 @@ int main(int argc, char **argv) {
     int opt; // get arguments in command flags (optarg)
     char diskname[100];
     char *filename = (char *) malloc(100 * sizeof(char));
-    char target_sha_hash[SHA_DIGEST_LENGTH * 2 + 1];
+    char target_sha_hash[41];
     int mode = -1;
     bool mode_s = false;
-
 
     // deal with some exceptions
     if (argc == 1 || strcmp(argv[1], "-i") == 0) {
@@ -58,16 +62,22 @@ int main(int argc, char **argv) {
                         printf(" ");
                     }
                 }
-                exit(1);
+                exit(1); // exit abnormally
         }
     }
-
    
     
     // Boot sector is the first 512 bytes of the FS
-    size_t boot_sector_size = 90; // like the lecture said
+    size_t boot_sector_size = 512;
     BootEntry *boot_sector_info;
     
+    /*
+        Using mmap() to access the disk image is more convenient than read() or fread(). 
+        You may need to open the disk image with O_RDWR and map it with PROT_READ | PROT_WRITE and MAP_SHARED in order to update the underlying file. 
+        Once you have mapped your disk image, you can cast any address to the FAT32 data structure type, 
+        such as (DirEntry *)(mapped_address + 0x5000). 
+        You can also cast the FAT to int[] for easy access.
+    */
     int diskd = open(diskname, O_RDWR);
     
     void *map_boot = mmap(NULL, boot_sector_size, PROT_READ | PROT_WRITE, MAP_SHARED, diskd, 0);
@@ -75,23 +85,27 @@ int main(int argc, char **argv) {
     boot_sector_info = (BootEntry *) map_boot;
     
     // get the end of reserved area
-    // fat_off (fat_start): the offset of fat table
-    int fat_off = (int) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_RsvdSecCnt);
-    size_t fat_size = (size_t) boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_FATSz32 * boot_sector_info->BPB_NumFATs;
+    // fat_start: the starting byte of fat table
+    int fat_start = (int) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_RsvdSecCnt);
+    size_t fat_size = (size_t) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_NumFATs * boot_sector_info->BPB_FATSz32);
 
-    // get the first byte of data area
-    int data_area_off = fat_off + ((int) fat_size); // equals to (DirEntry *)(mapped_address + 0x5000)
+    // get the first byte of data area (the start of the data_area)
+    // checked: equals to (DirEntry *)(mapped_address + 0x5000)
+    int data_area_start = fat_start + ((int) fat_size); 
     
     // get the size of the disk
     struct stat disk_stat;
     int return_ret = fstat(diskd, &disk_stat);
     char *disk_info = mmap(NULL, disk_stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, diskd, 0);
+    
     // clus_size: used in "Data Area"
-    // size_t clus_size = (size_t) boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_SecPerClus;
 
     // get the start of the fat entry
-    int *fat_entry_start = (int *) (disk_info + fat_off);
-    int root_cluster = boot_sector_info->BPB_RootClus; // usually 2
+    int *fat_entry_start = (int *) (disk_info + fat_start);
+    int root_dir_clus = boot_sector_info->BPB_RootClus; // usually 2
+
+    // place outside different modes
+    int clus_size = (int) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_SecPerClus); 
 
     if (mode == 1) {
         //
@@ -99,14 +113,18 @@ int main(int argc, char **argv) {
     }
     else if (mode == 2) {
         // print_directory_info
-        int next_fat_entry = root_cluster;
+        int next_dir_ent_clus = root_dir_clus;
 
+        // record the total number of entries
         int count_entries = 0;
-        int cluster_bytes = (int) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_SecPerClus);
-        int dir_count = cluster_bytes / sizeof(DirEntry);
+        
+        int dir_count = clus_size / sizeof(DirEntry);
 
-        while (next_fat_entry < 0x0ffffff8) {            
-            char* dir_loc = disk_info + data_area_off + (next_fat_entry - 2) * cluster_bytes;
+        // end of the clus: 0x0ffffff8
+        while (next_dir_ent_clus < 0x0ffffff8) {            
+            // because the root directory cluster starts from 2
+            char* dir_loc = disk_info + data_area_start + (next_dir_ent_clus - 2) * clus_size;
+
             // check the data area for this cluster
             DirEntry *dir_info = (DirEntry *) (dir_loc);
             
@@ -125,45 +143,47 @@ int main(int argc, char **argv) {
             }
 
             // index method
-            // printf("fat_entry_start[next_fat_entry]: %d\n", fat_entry_start[next_fat_entry]);
-            next_fat_entry = fat_entry_start[next_fat_entry];
+            // printf("fat_entry_start[next_dir_ent_clus]: %d\n", fat_entry_start[next_dir_ent_clus]);
+            next_dir_ent_clus = fat_entry_start[next_dir_ent_clus];
         }
         printf("Total number of entries = %d\n", count_entries);
     }
     else if (mode == 3) {
         // have filename here
         // ./nyufile fat32.disk -r HELLO.TXT
-        
-        // 
 
-        int next_fat_entry = root_cluster;
+        int next_dir_ent_clus = root_dir_clus;
 
-        int cluster_size = (int) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_SecPerClus);
-        int dir_count = cluster_size / sizeof(DirEntry);
+        int dir_count = clus_size / sizeof(DirEntry);
 
         //
         int num_contiguous = 0;
-        int num_ambiguous = 0, get_del_fat = -1, record_del_cluster = -1; 
+        int num_ambiguous = 0, del_dir_ent_clus = -1, record_id_del_clus = -1; 
         bool hash_found = false;
 
-        while (next_fat_entry < 0x0ffffff8 && hash_found == false) {            
-            //
-            char* dir_loc = disk_info + data_area_off + (next_fat_entry - 2) * cluster_size;
+        // if hash is found, break the while loop
+        while (next_dir_ent_clus < 0x0ffffff8 && hash_found == false) {            
+            // same as mode 2
+            char* dir_loc = disk_info + data_area_start + (next_dir_ent_clus - 2) * clus_size;
             DirEntry *dir_info = (DirEntry *) (dir_loc);
             
+            // logic same as mode 2
             for (int i = 0; i < dir_count; i++) {
                 if ((dir_info[i].DIR_Name)[0] != 0x00) {
                     if ((dir_info[i].DIR_Name)[0] == 0xe5) {
                         //
                         int filename_id = 1;
                         bool check_res = true;
-    
-                        for (int wd = 1; wd < 11; wd++) {
-                            if ((dir_info[i].DIR_Name)[wd] == ' ') {
+
+                        // for the deleted file, if the characters except for the first one
+                        // are all the same, mark that as potential directory to recover
+                        // !!! if no hash is provided !!!
+                        for (int wrd = 1; wrd < 11; wrd++) {
+                            if ((dir_info[i].DIR_Name)[wrd] == ' ') {
                                 continue;
                             }
     
-                            if ((dir_info[i].DIR_Name)[wd] != filename[filename_id]) {
+                            if ((dir_info[i].DIR_Name)[wrd] != filename[filename_id]) {
                                 check_res = false;
                                 break;
                             }
@@ -178,23 +198,28 @@ int main(int argc, char **argv) {
                         if (check_res == true) {
                             // if mode_s == true (SHA-1 is provided)
 
-                            // #define SHA_DIGEST_LENGTH 20
+                            // # define SHA_DIGEST_LENGTH 20
                             if (mode_s == true) {
                                 // turn the name back
                                 unsigned char new_sha_temp[SHA_DIGEST_LENGTH];
-                                char new_sha_hash[SHA_DIGEST_LENGTH * 2 + 1];
-                                int get_start_clus = (dir_info[i].DIR_FstClusHI << 16 | dir_info[i].DIR_FstClusLO);
+                                char new_sha_hash[41];
+                                unsigned short high_b = dir_info[i].DIR_FstClusHI;
+                                unsigned short low_b = dir_info[i].DIR_FstClusLO;
+
+                                int get_start_clus = (high_b << 16 | low_b);
                                 
                                 
                                 // check whether SHA-1 is the same
-                                SHA1(((unsigned char *) (dir_loc + (get_start_clus - 2) * cluster_size)), ((size_t) dir_info[i].DIR_FileSize), new_sha_temp);
+                                SHA1(((unsigned char *) (dir_loc + (get_start_clus - 2) * clus_size)), ((size_t) dir_info[i].DIR_FileSize), new_sha_temp);
                                 
+                                /* reference from stack overflow */
                                 for (int s = 0; s < SHA_DIGEST_LENGTH; s++) {
                                     sprintf(new_sha_hash + s*2, "%02x", new_sha_temp[s]);
                                 }
 
+                                //
                                 if (strcmp(((char *) new_sha_hash), ((char *) target_sha_hash)) == 0) {
-                                    num_contiguous = (dir_info[i].DIR_FileSize / cluster_size) + 1;
+                                    num_contiguous = (dir_info[i].DIR_FileSize / clus_size) + 1;
 
                                     if (num_contiguous <= 1) {
                                         (dir_info[i].DIR_Name)[0] = filename[0];
@@ -216,8 +241,8 @@ int main(int argc, char **argv) {
                             }
                             else {
                                 num_ambiguous++;
-                                get_del_fat = next_fat_entry;
-                                record_del_cluster = i;
+                                del_dir_ent_clus = next_dir_ent_clus;
+                                record_id_del_clus = i;
                                 check_res = false;
                             }
                         }
@@ -230,8 +255,9 @@ int main(int argc, char **argv) {
                     break;
                 }
             }
+
             if (hash_found == false) {
-                next_fat_entry = fat_entry_start[next_fat_entry];    
+                next_dir_ent_clus = fat_entry_start[next_dir_ent_clus];    
             }
             else {
                 break;
@@ -240,26 +266,30 @@ int main(int argc, char **argv) {
 
         // check ambiguity
         if (mode_s == false) {
-            if (num_ambiguous == 1) { 
-                // change the delete file character back to the original character
-                char* dir_loc = disk_info + data_area_off + (get_del_fat - 2) * cluster_size;
+            if (num_ambiguous == 1) {
+                char* dir_loc = disk_info + data_area_start + (del_dir_ent_clus - 2) * clus_size;
                 DirEntry *dir_info = (DirEntry *) (dir_loc);
+
+                unsigned short high_b = dir_info[record_id_del_clus].DIR_FstClusHI;
+                unsigned short low_b = dir_info[record_id_del_clus].DIR_FstClusLO;
+                
+                
+                // change the delete file character back to the original character
+                (dir_info[record_id_del_clus].DIR_Name)[0] = filename[0];
     
-                (dir_info[record_del_cluster].DIR_Name)[0] = filename[0];
+                next_dir_ent_clus = (high_b << 16) | low_b;
+                num_contiguous = (dir_info[record_id_del_clus].DIR_FileSize / clus_size) + 1;
     
-                next_fat_entry = (dir_info[record_del_cluster].DIR_FstClusHI << 16) | dir_info[record_del_cluster].DIR_FstClusLO;
-                num_contiguous = (dir_info[record_del_cluster].DIR_FileSize / cluster_size) + 1;
-    
-    
+                // check if the file is stored in multiple clusters
                 if (num_contiguous <= 1) {
-                    fat_entry_start[next_fat_entry] = 0x0ffffff8;
+                    fat_entry_start[next_dir_ent_clus] = 0x0ffffff8;
                     printf("%s: successfully recovered\n", filename);
                 }
                 else {
                     for (int f = 1; f < num_contiguous; f++) {
-                        fat_entry_start[next_fat_entry + f - 1] = next_fat_entry + f;                            
+                        fat_entry_start[next_dir_ent_clus + f - 1] = next_dir_ent_clus + f;                            
                     }
-                    fat_entry_start[next_fat_entry + num_contiguous - 1] = 0x0ffffff8;
+                    fat_entry_start[next_dir_ent_clus + num_contiguous - 1] = 0x0ffffff8;
                     printf("%s: successfully recovered\n", filename);
                    
                 }
@@ -280,102 +310,13 @@ int main(int argc, char **argv) {
         //
         exit(0);
 
-        // have filename here
-        // ./nyufile fat32.disk -r HELLO.TXT
-
-        //  correct code
-        
-        int next_fat_entry = root_cluster;
-
-        int cluster_size = (int) (boot_sector_info->BPB_BytsPerSec * boot_sector_info->BPB_SecPerClus);
-        int dir_count = cluster_size / sizeof(DirEntry);
-
-        //
-        int num_contiguous = 0;
-        int num_ambiguous = 0, get_del_fat = -1, record_del_cluster = -1; 
-
-        while (next_fat_entry < 0x0ffffff8) {
-            //
-            char* dir_loc = disk_info + data_area_off + (next_fat_entry - 2) * cluster_size;
-            DirEntry *dir_info = (DirEntry *) (dir_loc);
-            
-            for (int i = 0; i < dir_count; i++) {
-                if ((dir_info[i].DIR_Name)[0] != 0x00) {
-                    if ((dir_info[i].DIR_Name)[0] == 0xe5) {
-                        //
-                        int filename_id = 1;
-                        bool check_res = true;
-    
-                        for (int wd = 1; wd < 11; wd++) {
-                            if ((dir_info[i].DIR_Name)[wd] == ' ') {
-                                continue;
-                            }
-    
-                            if ((dir_info[i].DIR_Name)[wd] != filename[filename_id]) {
-                                check_res = false;
-                                break;
-                            }
-    
-                            filename_id++;
-                            if (filename[filename_id] == '.') {
-                                filename_id++;
-                            }
-                        }
-    
-                        // find the file and recover it
-                        if (check_res == true) {
-                            num_ambiguous++;
-                            get_del_fat = next_fat_entry;
-                            record_del_cluster = i;
-                            check_res = false;
-                        }
-                    }
-                    else {
-                        continue;
-                    }
-                }
-                else {
-                    break;
-                }
-            }
-            next_fat_entry = fat_entry_start[next_fat_entry];    
-        }
-
-        //
-        if (num_ambiguous == 1) {
-            // change the delete file character back to the original character
-            char* dir_loc = disk_info + data_area_off + (get_del_fat - 2) * cluster_size;
-            DirEntry *dir_info = (DirEntry *) (dir_loc);
-
-            (dir_info[record_del_cluster].DIR_Name)[0] = filename[0];
-
-            next_fat_entry = (dir_info[record_del_cluster].DIR_FstClusHI << 16) | dir_info[record_del_cluster].DIR_FstClusLO;
-            num_contiguous = (dir_info[record_del_cluster].DIR_FileSize / cluster_size) + 1;
-
-            if (num_contiguous <= 1) {
-                fat_entry_start[next_fat_entry] = 0x0ffffff8;
-                printf("%s: successfully recovered\n", filename);
-            }
-            else {
-                for (int f = 1; f < num_contiguous; f++) {
-                    fat_entry_start[next_fat_entry + f - 1] = next_fat_entry + f;                            
-                }
-                fat_entry_start[next_fat_entry + num_contiguous - 1] = 0x0ffffff8;
-                printf("%s: successfully recovered\n", filename);
-            }
-        }
-        else if (num_ambiguous > 1) {
-            printf("%s: multiple candidates found\n", filename);
-        }
-        else {
-            printf("%s: file not found\n", filename);
-        }
     }
     else {
         // fix the warning
-        printf("mode_s: %d, %d\n", mode_s, return_ret);
+        fprintf(stderr, "The mode does not exist.\n");
     }
 
-    //
+    // free the memories
+    free(filename);
     close(diskd);
 }
